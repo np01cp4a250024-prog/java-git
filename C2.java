4f0c8901bdfc5032b5d0d4e2e81d1bb4d251fac8
Aryan <np01cp4a250024@islingtoncollege.edu.np>
2025-11-19 12:22:22 +0545

    Initial sharing of project

---
+++ C2.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class C2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class C2
+{
+    public static void main(String[] args){
+    int age=18;
+    int qty=8;
+    
+    //implicit type casting
+    int h=10;
+    double k=h;
+    
+    double a=18.09;
+    int l=(int)a;
+    
+    long v=100000;
+    int u=(int)v;
+    System.out.println(u);
+    }
+}
