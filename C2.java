9aa9ae4945203a6411b3c7a136b57d2b61d5e6a9
Aryan <np01cp4a250024@islingtoncollege.edu.np>
2025-11-19 13:01:16 +0545

    2nd prog

--- C2.java
+++ C2.java
@@ -7,7 +7,18 @@
  */
 public class C2
 {
+    int age; //instance variable
+    static int agr; //static variable
     public static void main(String[] args){
+        int qty=10; //local variable
     System.out.println("it is our third workshop");
+    
+    //<class name> <variable>= new <class name>();
+    
+    C2 w1=new C2();
+    
+    System.out.println(w1.age);
+    System.out.println(agr);
+    System.out.println(qty);
     }
 }
---
+++ mathopt.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class mathopt here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class mathopt
+{
+    public static void main(String[] args){}
+}
