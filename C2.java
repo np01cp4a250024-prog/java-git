d0336bb212d63cade3ffe31b751a75fd676ed410
Aryan <np01cp4a250024@islingtoncollege.edu.np>
2025-11-19 12:42:00 +0545

    Simple statment updated

--- C2.java
+++ C2.java
@@ -8,18 +8,6 @@
 public class C2
 {
     public static void main(String[] args){
-    int age=18;
-    int qty=8;
-    
-    //implicit type casting
-    int h=10;
-    double k=h;
-    
-    double a=18.09;
-    int l=(int)a;
-    
-    long v=100000;
-    int u=(int)v;
-    System.out.println(u);
+    System.out.println("it is our third workshop");
     }
 }
